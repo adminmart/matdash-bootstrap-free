367fef5c0043af839dab55bf3f0c9a77a5aff52a
rutvik <wrappiexlteam.2016@gmail.com>
2024-03-22 18:00:19 +0530

    ''

---
+++ src/assets/js/dashboard.js
@@ -0,0 +1,209 @@
+$(function () {
+
+
+  // -----------------------------------------------------------------------
+  // Subscriptions
+  // -----------------------------------------------------------------------
+  var chart = {
+    series: [
+      {
+        name: "2024",
+        data: [1.2, 2.7, 1, 3.6, 2.1, 2.7, 2.2, 1.3, 2.5],
+      },
+      {
+        name: "2023",
+        data: [-2.8, -1.1, -2.5, -1.5, -2.3, -1.9, -1, -2.1, -1.3],
+      },
+    ],
+    chart: {
+      toolbar: {
+        show: false,
+      },
+      type: "bar",
+      fontFamily: "inherit",
+      foreColor: "#adb0bb",
+      height: 330,
+      stacked: true,
+      offsetX: -15,
+    },
+    colors: ["var(--bs-primary)", "var(--bs-danger)"],
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        barHeight: "60%",
+        columnWidth: "15%",
+        borderRadius: [6],
+        borderRadiusApplication: "end",
+        borderRadiusWhenStacked: "all",
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    legend: {
+      show: false,
+    },
+    grid: {
+      show: true,
+      padding: {
+        top: 0,
+        bottom: 0,
+        right: 0,
+      },
+      borderColor: "rgba(0,0,0,0.05)",
+      xaxis: {
+        lines: {
+          show: true,
+        },
+      },
+      yaxis: {
+        lines: {
+          show: true,
+        },
+      },
+    },
+    yaxis: {
+      min: -5,
+      max: 5,
+    },
+    xaxis: {
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+      categories: [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "July",
+        "Aug",
+        "Sep",
+      ],
+      labels: {
+        style: { fontSize: "13px", colors: "#adb0bb", fontWeight: "400" },
+      },
+    },
+    yaxis: {
+      tickAmount: 4,
+    },
+    tooltip: {
+      theme: "dark",
+    },
+  };
+
+  var chart = new ApexCharts(
+    document.querySelector("#revenue-forecast"),
+    chart
+  );
+  chart.render();
+
+
+  // =====================================
+  // Breakup
+  // =====================================
+  var breakup = {
+    color: "#adb5bd",
+    series: [38, 40, 25],
+    labels: ["2022", "2021", "2020"],
+    chart: {
+      width: 180,
+      type: "donut",
+      fontFamily: "inherit",
+      foreColor: "#adb0bb",
+    },
+    plotOptions: {
+      pie: {
+        startAngle: 0,
+        endAngle: 360,
+        donut: {
+          size: '75%',
+        },
+      },
+    },
+    stroke: {
+      show: false,
+    },
+
+    dataLabels: {
+      enabled: false,
+    },
+
+    legend: {
+      show: false,
+    },
+    colors: ["var(--bs-primary)", "#ecf2ff", "#F9F9FD"],
+
+    responsive: [
+      {
+        breakpoint: 991,
+        options: {
+          chart: {
+            width: 150,
+          },
+        },
+      },
+    ],
+    tooltip: {
+      theme: "dark",
+      fillSeriesColor: false,
+    },
+  };
+
+  var chart = new ApexCharts(document.querySelector("#breakup"), breakup);
+  chart.render();
+
+
+
+  // =====================================
+  // Earning
+  // =====================================
+  var earning = {
+    chart: {
+      id: "sparkline3",
+      type: "area",
+      height: 60,
+      sparkline: {
+        enabled: true,
+      },
+      group: "sparklines",
+      fontFamily: "inherit",
+      foreColor: "#adb0bb",
+    },
+    series: [
+      {
+        name: "Earnings",
+        color: "#46caeb",
+        data: [25, 66, 20, 40, 12, 58, 20],
+      },
+    ],
+    stroke: {
+      curve: "smooth",
+      width: 2,
+    },
+    fill: {
+      colors: ["#f3feff"],
+      type: "solid",
+      opacity: 0.05,
+    },
+
+    markers: {
+      size: 0,
+    },
+    tooltip: {
+      theme: "dark",
+      fixed: {
+        enabled: true,
+        position: "right",
+      },
+      x: {
+        show: false,
+      },
+    },
+  };
+  new ApexCharts(document.querySelector("#earning"), earning).render();
+})
